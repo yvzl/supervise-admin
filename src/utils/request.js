15d18a7578906a819d9eb83c54ef58c91556f006
yvzl <898101408@qq.com>
2026-03-06 22:40:54 +0800

    初始化项目

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,4 +6,28 @@ const request = axios.create({
     baseURL: baseUrl,
 })
 
+// 配置 axios 拦截器 - 每次请求前从 localStorage 读取 token
+request.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('admin_token');
+        console.log(token)
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// 响应拦截器
+request.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        console.error('API Error:', error.response?.data);
+        return Promise.reject(error);
+    }
+);
+
 export default request;
